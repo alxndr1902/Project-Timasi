e6b406bda54668c2529bb1fb84d1f9c73c423b64
Alexander <romian37@gmail.com>
2026-01-15 16:34:10 +0700

    initial commit

---
+++ src/main/java/com/zezame/timasi/ProjectTimasiApplication.java
@@ -0,0 +1,13 @@
+package com.zezame.timasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTimasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTimasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zezame/timasi/ProjectTimasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zezame.timasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTimasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

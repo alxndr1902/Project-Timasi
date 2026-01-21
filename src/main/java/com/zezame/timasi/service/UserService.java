21efb8cff71a797cefb6a27fbe261367fdf8af9b
Alex <romian37@gmail.com>
2026-01-21 22:25:10 +0700

    implement UserDetailService

--- src/main/java/com/zezame/timasi/service/UserService.java
+++ src/main/java/com/zezame/timasi/service/UserService.java
@@ -1,7 +1,8 @@
 package com.zezame.timasi.service;
 
 import com.zezame.timasi.model.company.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User findByEmail(String email);
 }
--- src/main/java/com/zezame/timasi/service/impl/UserServiceImpl.java
+++ src/main/java/com/zezame/timasi/service/impl/UserServiceImpl.java
@@ -2,12 +2,19 @@ package com.zezame.timasi.service.impl;
 
 import com.zezame.timasi.model.company.User;
 import com.zezame.timasi.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Override
     public User findByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
     }
 }

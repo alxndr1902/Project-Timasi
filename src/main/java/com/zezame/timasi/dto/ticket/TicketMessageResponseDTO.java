05922ec2ae04135de3e5277bbc36f8d77dc2b8da
Alex <romian37@gmail.com>
2026-01-23 07:10:39 +0700

    change to regular class

--- src/main/java/com/zezame/timasi/dto/ticket/TicketMessageResponseDTO.java
+++ src/main/java/com/zezame/timasi/dto/ticket/TicketMessageResponseDTO.java
@@ -3,6 +3,44 @@ package com.zezame.timasi.dto.ticket;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record TicketMessageResponseDTO(UUID id, String message, String senderName,
-                                       String roleName, LocalDateTime createdAt, Integer version) {
+public class TicketMessageResponseDTO {
+    private UUID id;
+    private String message;
+    private String senderName;
+    private String roleName;
+    private LocalDateTime createdAt;
+    private Integer version;
+
+    public TicketMessageResponseDTO(UUID id, String message, String senderName, String roleName, LocalDateTime createdAt, Integer version) {
+        this.id = id;
+        this.message = message;
+        this.senderName = senderName;
+        this.roleName = roleName;
+        this.createdAt = createdAt;
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
 }
--- src/main/java/com/zezame/timasi/dto/ticket/TicketStatusResponseDTO.java
+++ src/main/java/com/zezame/timasi/dto/ticket/TicketStatusResponseDTO.java
@@ -2,5 +2,26 @@ package com.zezame.timasi.dto.ticket;
 
 import java.util.UUID;
 
-public record TicketStatusResponseDTO(UUID id, String code, String name) {
+public class TicketStatusResponseDTO {
+    private UUID id;
+    private String code;
+    private String name;
+
+    public TicketStatusResponseDTO(UUID id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
